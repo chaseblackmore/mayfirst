c9eee8143f1ac8fc0f23f7ca95521f71b0fa9b08
blac0731 <blac0731@PCB-LAB360-40>
2019-05-01 11:35:32 -0500

    end of class

---
+++ review/review/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace review.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ review/review/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="review.WebApiApplication" Language="C#" %>
